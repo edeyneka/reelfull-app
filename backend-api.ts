e54702755fab8329d01f292e8f1adb9163457775
Aleksey Bykhun <theisease@gmail.com>
2025-10-10 16:17:17 -0700

    update api

--- backend-api.ts
+++ backend-api.ts
@@ -19,10 +19,21 @@ export type PublicApiType = {
       { files: Array<Id<"_storage">>; prompt: string },
       any
     >;
+    getProjects: FunctionReference<
+      "query",
+      "public",
+      Record<string, never>,
+      any
+    >;
+    getProject: FunctionReference<
+      "query",
+      "public",
+      { id: Id<"projects"> },
+      any
+    >;
   };
   render: {
     render: FunctionReference<"action", "public", { projectId: string }, any>;
   };
 };
 export type InternalApiType = {};
-
