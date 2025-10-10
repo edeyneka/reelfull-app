4272e9439900af1f71f6cd82bc7ef00442f8f80d
Aleksey Bykhun <theisease@gmail.com>
2025-10-10 16:06:20 -0700

    change convex to use api.ts

---
+++ backend-api.ts
@@ -0,0 +1,28 @@
+import { type FunctionReference, anyApi } from "convex/server";
+import { type GenericId as Id } from "convex/values";
+
+export const api: PublicApiType = anyApi as unknown as PublicApiType;
+export const internal: InternalApiType = anyApi as unknown as InternalApiType;
+
+export type PublicApiType = {
+  tasks: {
+    get: FunctionReference<"query", "public", Record<string, never>, any>;
+    generateUploadUrl: FunctionReference<
+      "mutation",
+      "public",
+      Record<string, never>,
+      any
+    >;
+    createProject: FunctionReference<
+      "mutation",
+      "public",
+      { files: Array<Id<"_storage">>; prompt: string },
+      any
+    >;
+  };
+  render: {
+    render: FunctionReference<"action", "public", { projectId: string }, any>;
+  };
+};
+export type InternalApiType = {};
+
